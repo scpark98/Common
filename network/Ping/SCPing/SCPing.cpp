b0ad6478fd54d80ced93ff695af30206566ebbc9
scpark98 <scpark98@gmail.com>
2024-04-02 10:31:55 +0900

    .

---
+++ network/Ping/SCPing/SCPing.cpp
@@ -0,0 +1,234 @@
+#include "SCPing.h"
+
+#include <thread>
+#include <WinSock2.h>
+#include <IcmpAPI.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "Iphlpapi.lib")
+#pragma comment(lib, "Ws2_32.lib")
+
+#pragma warning(disable:4996)
+
+CSCPing::CSCPing()
+{
+	WSAData wsaData;
+
+	if (WSAStartup(MAKEWORD(1, 1), &wsaData) != 0)
+	{
+		AfxMessageBox(_T("WSAStartup failed. Could not initialize socket."));
+		return;
+	}
+}
+
+CSCPing::~CSCPing()
+{
+	WSACleanup();
+}
+
+bool CSCPing::init(HWND hParent)
+{
+	m_parent = hParent;
+	return true;
+}
+
+void CSCPing::add(CString addr, int retry, int timeout, CString job_name)
+{
+	CSCPingParams *params = new CSCPingParams(addr, retry, timeout, job_name);
+
+	std::map<CString, int>::iterator it = m_map.find(params->job_name);
+	if (it != m_map.end())
+		m_map[params->job_name] = PING_THREAD_IS_RUNNING;
+	else
+		m_map.insert(std::pair<CString, bool>(params->job_name, PING_THREAD_IS_RUNNING));
+
+	std::thread t(&CSCPing::thread_ping, this, params);
+	t.detach();
+}
+
+//해당 인덱스의 핑쓰레드를 중지. -1이면 모든 핑 중지.
+void CSCPing::stop(CString job_name)
+{
+	std::map<CString, int>::iterator it = m_map.find(job_name);
+
+	if (it != m_map.end() && it->second == PING_THREAD_IS_RUNNING)
+	{
+		it->second = PING_THREAD_IS_STOPPING;
+		return;
+	}
+
+	//모두 중지할 경우
+	for (it = m_map.begin(); it != m_map.end(); it++)
+	{
+		if (it->second == PING_THREAD_IS_RUNNING)
+			it->second = PING_THREAD_IS_STOPPING;
+	}
+
+	//while (!is_all_threads_stopped())
+	//{
+	//	TRACE(_T("wait all threads stopped...\n"));
+	//	//Sleep(1000);
+	//}
+}
+
+bool CSCPing::is_all_threads_stopped()
+{
+	for (auto it = m_map.begin(); it != m_map.end(); it++)
+	{
+		if (it->second != PING_THREAD_NOT_RUNNING)
+			return false;
+	}
+
+	return true;
+}
+
+CString CSCPing::get_ip_error_string(DWORD error_code)
+{
+	//auto e = GetLastError();
+	DWORD buf_size = 1024;
+	TCHAR buf[1024] = { 0, };
+	GetIpErrorString(error_code, buf, &buf_size);
+
+	return CString(buf);
+}
+
+void CSCPing::thread_ping(CSCPingParams* params)
+{
+	CString str;
+
+	// Create the ICMP context.
+	HANDLE icmp_handle = IcmpCreateFile();
+	if (icmp_handle == INVALID_HANDLE_VALUE)
+	{
+		m_map[params->job_name] = PING_THREAD_NOT_RUNNING;
+		params->status = PING_STATUS_WSA_ERROR;
+		::SendMessage(m_parent, Message_CSCPing, (WPARAM)params, (LPARAM)0);
+		delete params;
+		return;
+	}
+
+	// Parse the destination IP address.
+	IN_ADDR dest_ip{};
+	LPHOSTENT lpHost = gethostbyname(CT2CA(params->addr));
+	if (lpHost == NULL)
+	{
+		m_map[params->job_name] = PING_THREAD_NOT_RUNNING;
+		auto e = GetLastError();
+		DWORD buf_size = 1024;
+		TCHAR buf[1024] = { 0, };
+		GetIpErrorString(e, buf, &buf_size);
+
+		str = get_ip_error_string(GetLastError());
+		params->status = PING_STATUS_HOST_NOT_FOUND;
+		::SendMessage(m_parent, Message_CSCPing, (WPARAM)params, (LPARAM)0);
+		delete params;
+		return;
+	}
+
+	struct sockaddr_in sa_dst;
+	sa_dst.sin_addr.s_addr = *((u_long FAR*) (lpHost->h_addr));
+	sa_dst.sin_family = AF_INET;
+	sa_dst.sin_port = 0;
+
+	if (1 != InetPton(AF_INET, CString(inet_ntoa(sa_dst.sin_addr)), &dest_ip))
+	{
+		m_map[params->job_name] = PING_THREAD_NOT_RUNNING;
+		params->status = PING_STATUS_NOT_DEFINED;
+		::SendMessage(m_parent, Message_CSCPing, (WPARAM)params, (LPARAM)0);
+
+		TRACE(_T("InetPton failed.\n"));
+		throw;
+	}
+
+	params->addr_ip = CString(inet_ntoa(sa_dst.sin_addr));
+
+	// Payload to send.
+	constexpr WORD payload_size = 32;
+	unsigned char payload[payload_size]{ 42 };
+
+	params->payload_size = payload_size;
+
+
+	// Reply buffer for exactly 1 echo reply, payload data, and 8 bytes for ICMP error message.
+	constexpr DWORD reply_buf_size = sizeof(ICMP_ECHO_REPLY) + payload_size + 8;
+	unsigned char reply_buf[reply_buf_size]{};
+
+	params->retry_count = 0;
+
+	//start signal
+	params->status = PING_STATUS_START_SIGNAL;
+	::SendMessage(m_parent, Message_CSCPing, (WPARAM)params, (LPARAM)0);
+
+	while ((m_map[params->job_name] == PING_THREAD_IS_RUNNING) &&
+		   (params->retry_total < 0 || (params->retry_count < params->retry_total)))
+	{
+		params->retry_count++;
+
+		long t0 = clock();
+
+		// Make the echo request.
+		DWORD reply_count = IcmpSendEcho(icmp_handle, dest_ip.S_un.S_addr,
+			payload, payload_size, NULL, reply_buf, reply_buf_size, params->timeout);
+
+		// Return value of 0 indicates failure, try to get error info.
+		if (reply_count == 0)
+		{
+			params->delay = clock() - t0;
+
+			auto e = GetLastError();
+			DWORD buf_size = 1000;
+			WCHAR buf[1000];
+			GetIpErrorString(e, buf, &buf_size);
+
+			//IP_REQ_TIMED_OUT
+			//str.Format(_T("IcmpSendEcho returned error %d (%s)"), e, buf);
+			//TRACE(_T("%s\n"), str);
+			if (e == IP_REQ_TIMED_OUT)
+				params->status = PING_STATUS_REQUEST_TIMED_OUT;
+			else
+				params->status = e;
+			::SendMessage(m_parent, Message_CSCPing, (WPARAM)params, (LPARAM)0);
+		}
+		else
+		{
+			const ICMP_ECHO_REPLY* r = (const ICMP_ECHO_REPLY*)reply_buf;
+
+			params->delay = r->RoundTripTime;
+
+			struct in_addr addr;
+			addr.s_addr = r->Address;
+			char* s_ip = inet_ntoa(addr);
+
+			if (CString(s_ip) == params->addr_ip)
+			{
+				params->TTL = (int)r->Options.Ttl;
+				params->status = PING_STATUS_ALIVE;
+				::SendMessage(m_parent, Message_CSCPing, (WPARAM)params, (LPARAM)0);
+
+				//str.Format(_T("Reply from: %s: bytes=%d time=%d ms TTL=%d"), CString(s_ip), r->DataSize, r->RoundTripTime, (int)r->Options.Ttl);
+				//TRACE(_T("%s\n"), str);
+			}
+			else
+			{
+				params->status = PING_STATUS_REQUEST_TIMED_OUT;
+				::SendMessage(m_parent, Message_CSCPing, (WPARAM)params, (LPARAM)0);
+			}
+
+			//std::cout << "Reply from: " << s_ip << ": bytes=" << r->DataSize << " time=" << r->RoundTripTime << "ms TTL=" << (int)r->Options.Ttl << std::endl;
+		}
+
+		if (params->delay >= 0 && params->delay < 1000)
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000 - params->delay));
+
+		//std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+
+	// Close ICMP context.
+	IcmpCloseHandle(icmp_handle);
+
+	m_map[params->job_name] = PING_THREAD_NOT_RUNNING;
+
+	TRACE(_T("ping %s stopped.\n"), params->job_name);
+
+	delete params;
+}
---
+++ network/Ping/SCPing/SCPing.h
@@ -0,0 +1,172 @@
+#pragma once
+
+#ifndef __SCPING__
+#define __SCPING__
+
+#include <afxcmn.h>
+#include <deque>
+#include <atomic>
+#include <chrono>
+#include <future>
+#include <map>
+
+#pragma pack(1)
+
+#define ICMP_ECHOREPLY	0
+#define ICMP_ECHOREQ	8
+
+static const UINT Message_CSCPing = ::RegisterWindowMessage(_T("MessageString_CSCPing"));
+
+// stopwatch. Returns time in seconds
+class timer {
+public:
+	std::chrono::time_point<std::chrono::high_resolution_clock> lastTime;
+	timer() : lastTime(std::chrono::high_resolution_clock::now()) {}
+	inline double elapsed() {
+		std::chrono::time_point<std::chrono::high_resolution_clock> thisTime = std::chrono::high_resolution_clock::now();
+		double deltaTime = std::chrono::duration<double>(thisTime - lastTime).count();
+		lastTime = thisTime;
+		return deltaTime;
+	}
+};
+
+class CSCPing;
+
+class CSCPingParams
+{
+public:
+	CSCPingParams() {};
+	CSCPingParams(CString _addr, int _retry_total, int _timeout = 5000, CString _job_name = _T(""), int _status = -2, int _retry_count = -1, int _delay = 0)
+	{
+		addr = _addr;
+		retry_total = _retry_total;
+		timeout = _timeout;
+		job_name = (_job_name.IsEmpty() ? addr : _job_name);
+	}
+
+	//input
+
+	//id로 각 ping job을 구분하려 했으나 그 아이디는 항상 unique해야하고 변경되지 않아야하므로 제약이 있을 수 있다.
+	//예를 들어 listctrl의 index를 id로 줬는데 sort를 한다면 id는 의미 없어진다.
+	//따라서 addr을 key로 한다.
+	//int			id = -1;
+
+	CString		addr = _T("");			//domain name or ip address
+	int			retry_total = -1;		//-1 : infinite
+	int			timeout = 5000;			//ms
+	//job을 구분하는 키로서 ""이면 addr값을 복사해서 사용한다.
+	//몇 개 정도의 ping job을 돌린다면 생략해도 되지만
+	//addr이 중복될 수 있는 경우는 반드시 명시해야 결과값을 구분할 수 있다.
+	CString		job_name;
+
+
+	//out
+	CString		addr_ip = _T("");		//converted ip address
+	int			status = -2;
+	int			retry_count = -1;
+	int			delay = 0;
+	int			payload_size = 0;
+	int			TTL = 0;
+
+};
+
+class CSCPing
+{
+public:
+	CSCPing();
+	~CSCPing();
+
+	enum PING_STATUS
+	{
+		PING_STATUS_WSA_ERROR = -1,
+		PING_STATUS_HOST_NOT_FOUND,
+		PING_STATUS_START_SIGNAL,
+		PING_STATUS_REQUEST_TIMED_OUT,
+		PING_STATUS_ALIVE,
+		PING_STATUS_END_SIGNAL,
+		PING_STATUS_NOT_DEFINED,
+		PING_STATUS_SOCKET_ERROR,
+	};
+
+	enum PING_THREAD_STATUS
+	{
+		PING_THREAD_NOT_RUNNING = -1,	//thread가 아직 시작되지 않은 상태
+		PING_THREAD_IS_STOPPING,		//취소 명령으로 중지중인 상태
+		PING_THREAD_IS_RUNNING,			//정상적으로 실행중인 상태
+	};
+
+	bool	init(HWND hParent);
+	void	add(CString addr, int retry = -1, int timeout = 5000, CString _job_name = _T(""));
+	void	thread_ping(CSCPingParams* params);
+
+	//해당 주소의 핑 쓰레드를 중지. ""이면 모든 핑 중지.
+	void	stop(CString job_name = _T(""));
+	bool	is_all_threads_stopped();
+
+protected:
+	HWND m_parent = NULL;
+
+	//id와 thread 동작 유무 저장 맵. 중지시킬 때 필요. 또한 모두 중지되었는지 확인시에도 필요.
+	//1:running, 0:stop thread, -1:stopped
+	std::map<CString, int> m_map;
+
+	int		WaitForEchoReply(SOCKET s);
+	// ICMP Echo Request/Reply functions
+	int		SendEchoRequest(SOCKET, LPSOCKADDR_IN);
+	DWORD	RecvEchoReply(SOCKET, LPSOCKADDR_IN, u_char*);
+	u_short in_cksum(u_short* addr, int len);
+	void	WSAError(LPCSTR pstrFrom);
+
+	CString get_ip_error_string(DWORD error_code);
+};
+
+// IP Header -- RFC 791
+typedef struct tagIPHDR
+{
+	u_char  VIHL;			// Version and IHL
+	u_char	TOS;			// Type Of Service
+	short	TotLen;			// Total Length
+	short	ID;				// Identification
+	short	FlagOff;		// Flags and Fragment Offset
+	u_char	TTL;			// Time To Live
+	u_char	Protocol;		// Protocol
+	u_short	Checksum;		// Checksum
+	struct	in_addr iaSrc;	// Internet Address - Source
+	struct	in_addr iaDst;	// Internet Address - Destination
+}IPHDR, * PIPHDR;
+
+
+// ICMP Header - RFC 792
+typedef struct tagICMPHDR
+{
+	u_char	Type;			// Type
+	u_char	Code;			// Code
+	u_short	Checksum;		// Checksum
+	u_short	ID;				// Identification
+	u_short	Seq;			// Sequence
+	char	Data;			// Data
+}ICMPHDR, * PICMPHDR;
+
+
+#define REQ_DATASIZE 32		// Echo Request Data size
+
+// ICMP Echo Request
+typedef struct tagECHOREQUEST
+{
+	ICMPHDR icmpHdr;
+	DWORD	dwTime;
+	char	cData[REQ_DATASIZE];
+}ECHOREQUEST, * PECHOREQUEST;
+
+
+// ICMP Echo Reply
+typedef struct tagECHOREPLY
+{
+	IPHDR	ipHdr;
+	ECHOREQUEST	echoRequest;
+	char    cFiller[256];
+}ECHOREPLY, * PECHOREPLY;
+
+#pragma pack()
+
+#endif

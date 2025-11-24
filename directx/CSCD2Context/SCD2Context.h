6962d6c3d99e15a85786d0fb6cf3c93089495625
scpark98 <scpark98@gmail.com>
2025-11-24 18:01:58 +0900

    .

--- directx/CSCD2Context/SCD2Context.h
+++ directx/CSCD2Context/SCD2Context.h
@@ -12,6 +12,10 @@
 ComPtr::operator&의 잘못된 사용 : operator&는 ComPtr가 관리하는 인터페이스를 먼저 해제한 다음, 해당 주소를 검색합니다.
 ComPtr를 in / out 매개변수로 전달할 때 GetAddressOf() 대신 operator&를 사용하면 기존 포인터가 의도치 않게 해제될 수 있습니다.
 해결책 : 함수가 포인터의 주소를 받으면서 기존 포인터를 보존해야 하는 경우 ComPtr::GetAddressOf()를 사용하십시오.
+
+[CSCD2Context를 MFC Control과 함께 사용할 경우]
+- 해당 윈도우에서 반드시 Clip Children을 해줘야만 Invalidate()에서 Control들이 깜빡이지 않는다.
+
 */
 
 using namespace Microsoft::WRL;
